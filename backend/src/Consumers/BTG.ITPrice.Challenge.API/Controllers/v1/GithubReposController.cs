a59041976ba40d7dda14e1ea2f4817b5ef7ee7de
Grazie Kranz <grazi.m.kranz@gmail.com>
2023-05-01 10:54:15 -0300

    refact: code clean and controller version

--- backend/src/Consumers/BTG.ITPrice.Challenge.API/Controllers/v1/GithubReposController.cs
+++ backend/src/Consumers/BTG.ITPrice.Challenge.API/Controllers/v1/GithubReposController.cs
@@ -2,14 +2,12 @@
 using Application.Services.Contracts;
 using BTG.ITPrice.Challenge.Infrastucture.Refit.Entities;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace BTG.ITPrice.Challenge.API.Controllers
+namespace BTG.ITPrice.Challenge.API.Controllers.v1
 {
     [ApiController]
-    [Route("btg/github-repos")]
+    [Route("v1.0/btg/github-repos")]
     public class GithubReposController : ControllerBase
     {
         private readonly IGithubReposService _service;
@@ -17,21 +15,14 @@ namespace BTG.ITPrice.Challenge.API.Controllers
         public GithubReposController(
             IGithubReposService service)
         {
-            _service= service;
+            _service = service;
         }
 
         [HttpGet("by-languages")]
-        public async Task<ActionResult<GithubReposResponse>> Get([FromQuery]GithubRepoRequest githubReposRequest)
+        public async Task<ActionResult<GithubReposResponse>> Get([FromQuery] GithubRepoRequest githubReposRequest)
         {
             var response = await _service.GetReposGithub(githubReposRequest);
             return Ok(response);
-        }
- 
-
-        [HttpGet]
-        public async Task<string> d()
-        {
-            return "teste";
         }
     }
 }

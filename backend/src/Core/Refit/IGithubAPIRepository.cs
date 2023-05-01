e78a0a3e0314a0c30afa8d63e2aa85233ea0b6be
Grazie Kranz <grazi.m.kranz@gmail.com>
2023-05-01 00:55:46 -0300

    feat: started refit structure

---
+++ backend/src/Core/Refit/IGithubAPIRepository.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Refit;
+using System.Threading.Tasks;
+
+namespace BTG.ITPrice.Challenge.Infrastucture.Refit
+
+{
+    public interface IGithubAPIRepository
+    {
+        [Get("/v1/cars")]
+        Task<object> ReturnCar();
+
+    }
+}

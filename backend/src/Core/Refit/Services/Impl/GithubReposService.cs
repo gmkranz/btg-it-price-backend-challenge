a5d9c5eb5cfba8fc438e082f52a466dce1053bb8
Grazie Kranz <grazi.m.kranz@gmail.com>
2023-05-01 04:49:30 -0300

    feat: add request handling and test

--- backend/src/Core/Refit/Services/Impl/GithubReposService.cs
+++ backend/src/Core/Refit/Services/Impl/GithubReposService.cs
@@ -10,7 +10,8 @@ namespace Application.Services.Impl
     public class GithubReposService : IGithubReposService
     {
         private const int MAX_LANGUAGES = 5;
-        private const string PATTERN_REGEX = @"[-+&]+";
+        //TODO: improve regex
+        private const string PATTERN_REGEX = @"[-&]+|&&?,";
         private const string PATTERN_REPLACE_REGEX = ",";
 
 
@@ -21,14 +22,51 @@ namespace Application.Services.Impl
             throw new NotImplementedException();
         }
 
-  
 
-        private string StandardLanguageRequest(string languages)
+
+        public static string StandardLanguageRequest(string languages)
         {
             ValidateLanguagesRequest(languages);
 
-            return Regex.Replace(languages, PATTERN_REGEX, PATTERN_REPLACE_REGEX);
+            var query = Regex.Replace(languages, PATTERN_REGEX, PATTERN_REPLACE_REGEX);
+            return query.EndsWith(PATTERN_REPLACE_REGEX) ? query.Remove(query.Length - 1) : query;
         }
 
+        public static void ValidateLanguagesRequest(string languages)
+        {
+            // separa as linguagens por vírgula
+            string[] languagesArray = languages.Split(",");
+
+            // se mais de 5 linguagens forem informadas, joga uma exceção
+
+            IsMaxLanguagesRequest(languagesArray);
+            if (languagesArray.Length > MAX_LANGUAGES)
+            {
+                throw new ArgumentException("O número máximo de linguagens permitido é 5.");
+            }
+
+            // substitui os caracteres '-' e '&' por ','
+            for (int i = 0; i < languagesArray.Length; i++)
+            {
+                languagesArray[i] = languagesArray[i].Replace("-", ",").Replace("&", ",");
+            }
+
+            // junta as linguagens novamente em uma string separada por vírgula
+            languages = string.Join(",", languagesArray);
+        }
+
+        private static void IsMaxLanguagesRequest(string[] languagesArray)
+        {
+            if (languagesArray.Length > MAX_LANGUAGES)
+
+                throw new ArgumentException("O número máximo de linguagens permitido é 5.");
+
+        }
+
+        public string GetReposGitheub(GithubRepoRequest request)
+        {
+            string teste = StandardLanguageRequest(request.Languages);
+            return teste;
+        }
     }
 }
---
+++ backend/test/UnitTests/UnitTests.Core.Application/GithubReposServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using Application.Services.Impl;
+using AutoFixture;
+using NUnit.Framework;
+
+namespace UnitTests.Core.Application
+{
+    [TestFixture]
+    [TestOf(typeof(GithubReposServiceTests))]
+    public class GithubReposServiceTests
+    {
+
+        IFixture _fixture;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fixture = new Fixture();
+        }
+
+        [TestCase("C#", "C#")]
+        [TestCase("JavaScript,", "JavaScript")]
+        [TestCase("Python&Java", "Python,Java")]
+        [TestCase("Go&Ruby&C++", "Go,Ruby,C++")]
+        public void StandardLanguageRequest_WithValidLanguages_ReturnsExpectedResult(string languages, string expected)
+        {
+            // Arrange
+            var service = _fixture.Create<GithubReposService>();
+
+            var result = GithubReposService.StandardLanguageRequest(languages);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+    }
+}

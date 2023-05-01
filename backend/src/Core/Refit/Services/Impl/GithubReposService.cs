612a85a5f71ce6e25a9d81c44c4a6e3468ebdef5
Grazie Kranz <grazi.m.kranz@gmail.com>
2023-05-01 07:41:11 -0300

    refact:upd validations request

--- backend/src/Core/Refit/Services/Impl/GithubReposService.cs
+++ backend/src/Core/Refit/Services/Impl/GithubReposService.cs
@@ -3,6 +3,7 @@ using Application.Services.Contracts;
 using BTG.ITPrice.Challenge.Infrastucture.Refit.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -14,8 +15,10 @@ namespace Application.Services.Impl
 
         private const int MAX_LANGUAGES = 5;
         //TODO: improve regex
-        private const string PATTERN_REGEX = @"[-&]+|&&?,";
+        private const string QUERY_LANGUAGE = "language:";
         private const string PATTERN_REPLACE_REGEX = ",";
+        private const string OR_OPERATOR = "+OR+";
+
 
         public GithubReposService(
             IGithubAPIRepository repositoryRefit
@@ -30,15 +33,16 @@ namespace Application.Services.Impl
 
             try
             {
-                var response = await _repositoryRefit.GetRepos(languagesQuery, request.PerPage.ToString(), request.Page.ToString());
-                if (response != null)
-                {
-                    return response;
-                }
-                else
-                {
-                    throw new Exception($"Failed to get Github repositories. Status code: {response}");
-                }
+                //var response = await _repositoryRefit.GetRepos(languagesQuery, request.PerPage.ToString(), request.Page.ToString());
+                //if (response != null)
+                //{
+                //    return response;
+                //}
+                //else
+                //{
+                //    throw new Exception($"Failed to get Github repositories. Status code: {response}");
+                //}
+                throw new Exception($"Failed to get Github repositories. Sta");
 
             }
             catch (Exception ex)
@@ -55,8 +59,29 @@ namespace Application.Services.Impl
         {
             ValidateLanguagesRequest(languages);
 
-            var query = Regex.Replace(languages, PATTERN_REGEX, PATTERN_REPLACE_REGEX);
-            return query.EndsWith(PATTERN_REPLACE_REGEX) ? query.Remove(query.Length - 1) : query;
+            if (languages.EndsWith(PATTERN_REPLACE_REGEX))
+                languages = languages.Remove(languages.Length - 1);
+
+            string[] listLanguages = languages.Split(PATTERN_REPLACE_REGEX);
+
+            if (listLanguages.Length == 1)
+            {
+                return $"{QUERY_LANGUAGE}{languages}";
+            }
+
+            string query = "";
+
+            for (int i = 0; i < listLanguages.Length; i++)
+            {
+                query += QUERY_LANGUAGE + listLanguages[i];
+
+                if (i < listLanguages.Length - 1)
+                {
+                    query += OR_OPERATOR;
+                }
+            }
+
+            return query;
         }
 
         public static void ValidateLanguagesRequest(string languages)
--- backend/test/UnitTests/UnitTests.Core.Application/GithubReposServiceTests.cs
+++ backend/test/UnitTests/UnitTests.Core.Application/GithubReposServiceTests.cs
@@ -17,19 +17,14 @@ namespace UnitTests.Core.Application
         {
             _fixture = new Fixture();
         }
-
-        [TestCase("C#", "C#")]
-        [TestCase("JavaScript,", "JavaScript")]
-        [TestCase("Python&Java", "Python,Java")]
-        [TestCase("Go&Ruby&C++", "Go,Ruby,C++")]
-        public void Should_StandardLanguageRequest_WithValidLanguages_ReturnsExpectedResult(string languages, string expected)
-        {
-            var service = _fixture.Create<GithubReposService>();
-
-            var result = GithubReposService.StandardLanguageRequest(languages);
-
-            Assert.That(result, Is.EqualTo(expected));
-        }
+[TestCase("JavaScript,", "language:JavaScript")]
+[TestCase("Python,Java", "language:Python+OR+language:Java")]
+[TestCase("Go,Ruby,C++", "language:Go+OR+language:Ruby+OR+language:C++")]
+public void Should_StandardLanguageRequest_WithValidLanguages_ReturnsExpectedResult(string languages, string expected)
+{
+    var result = GithubReposService.StandardLanguageRequest(languages);
+    Assert.That(result, Is.EqualTo(expected));
+}
         [Test]
         public void Should_IsMaxLanguagesRequest_WithTooManyLanguages_ThrowsArgumentException()
         {
